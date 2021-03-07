5a9596b6d91416608f8fcf542e6c233357d80644
Bartek Zawadka <kontakt@bartoszzawadka.pl>
2021-03-07 18:12:37 +0100

    Added fuzzy search

--- index.php
+++ index.php
@@ -3,56 +3,88 @@
 use Elasticsearch\ClientBuilder;
 
 require 'vendor/autoload.php';
-    $hosts = [
-        'http://bz-elastic.westeurope.azurecontainer.io:9200'
+$hosts = [
+    'http://bz-elastic.westeurope.azurecontainer.io:9200'
+];
+$client = ClientBuilder::create()
+    ->setHosts($hosts)
+    ->build();
+
+function addDocument($data)
+{
+    $entry = [
+        'index' => 'test',
+        'type' => 'test_type',
+        'body' => $data
     ];
-    $client = ClientBuilder::create()
-        ->setHosts($hosts)
-        ->build();
 
-    function addDocument($data){
-        $entry = [
-            'index' => 'test',
-            'type'  => 'test_type',
-            'body'  => $data
-        ];
+    $response = $GLOBALS['client']->index($entry);
+    echo json_encode($response);
+}
 
-        $response = $GLOBALS['client']->index($entry);
-        echo json_encode($response);
-    }
+function findDocuments($search)
+{
+    $params = [
+        'index' => 'test',
+        'type' => 'test_type',
+        'body' => [
+            'query' => [
+                'query_string' => [
+                    'query' => '*' . $search . '*',
+                    'fields' => ['firstName', 'lastName', 'description']
+                ]
+            ]
+        ]
+    ];
 
-    function findDocuments($search) {
-        $params = [
-            'index' => 'test',
-            'type' => 'test_type',
-            'body' => [
-                'query' => [
-                    'query_string' => [
-                        'query' => '*' . $search . '*',
-                        'fields' => ['firstName', 'lastName', 'description']
+    $c = $GLOBALS['client'];
+    $results = $c->search($params);
+    echo json_encode($results);
+}
+
+function findDocumentsFuzzy($search)
+{
+    $params = [
+        'index' => 'test',
+        'type' => 'test_type',
+        'body' => [
+            'query' => [
+                'match' => [
+                    'description' => [
+                        'query' => $search,
+                        'operator' => 'and',
+                        'fuzziness' => 2
                     ]
                 ]
             ]
-        ];
+        ]
+    ];
 
-        $c = $GLOBALS['client'];
-        $results = $c->search($params);
-        echo json_encode($results);
-    }
+    $c = $GLOBALS['client'];
+    $results = $c->search($params);
+    echo json_encode($results);
+}
 
-    if($_SERVER['REQUEST_METHOD'] == "POST" and (isset($_POST['action'])))
-    {
-        switch ($_POST['action']){
-            case "addDocument":
-                addDocument($_POST['data']);
-                break;
-            case "search":
-                $searchPhrase = '';
-                if(isset($_POST['value'])) {
-                    $searchPhrase = $_POST['value'];
-                }
-                findDocuments($searchPhrase);
-                break;
-        }
+if ($_SERVER['REQUEST_METHOD'] == "POST" and (isset($_POST['action']))) {
+    switch ($_POST['action']) {
+        case "addDocument":
+            addDocument($_POST['data']);
+            break;
+        case "search":
+            $searchPhrase = '';
+            if (isset($_POST['value'])) {
+                $searchPhrase = $_POST['value'];
+            }
+
+            findDocuments($searchPhrase);
+            break;
+        case "searchFuzzy":
+            $searchPhrase = '';
+            if (isset($_POST['value'])) {
+                $searchPhrase = $_POST['value'];
+            }
+
+            findDocumentsFuzzy($searchPhrase);
     }
+}
 
